045b61295ab47b0da8a8d274713402ad69cf2b6d
Yuliya <martjull@mail.ru>
2023-04-03 12:55:07 +0300

    Переносит всю разметку в js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const buttonElement = document.getElementById('add-button');
-const listElement = document.getElementById('list');
-const inputNameElement = document.getElementById('name-input');
-const inputTextElement = document.getElementById('text-input');
+
 let comments = [];
 const host = 'https://webdev-hw-api.vercel.app/api/v1/yuliya-martoshenko/comments'
 
@@ -18,7 +15,9 @@ const getDate = (date) => {
 };
 
 //рендер данных
-const renderComments = () => {
+const renderApp = () => {
+    const appElement = document.getElementById('app');
+
     const commentsHtml = comments.map((comment, index) => {
         return `<li class="comment">
         <div class="comment-header">
@@ -40,11 +39,115 @@ const renderComments = () => {
             <button data-index=${index} class=${comment.isLiked ? "'like-button -active-like'" : "'like-button'"}></button>
           </div>
         </div>
-  `
+    `
     }).join('');
-    listElement.innerHTML = commentsHtml;
+
+    const appHtml = `<div class="container">
+                <ul class="comments" id='list'>
+                ${commentsHtml}
+                </ul>
+                <div class="add-form">
+                <input
+                    type="text"
+                    class="add-form-name"
+                    placeholder="Введите ваше имя"
+                    id="name-input"
+                />
+                <textarea
+                    type="textarea"
+                    class="add-form-text"
+                    placeholder="Введите ваш коментарий"
+                    rows="4"
+                    id="text-input"
+                ></textarea>
+                <div class="add-form-row">
+                    <button class="add-form-button" id="add-button">Написать</button>
+                </div>
+                </div>
+                </div>`
+
+    appElement.innerHTML = appHtml;
+
+    const buttonElement = document.getElementById('add-button');
+    const inputNameElement = document.getElementById('name-input');
+    const inputTextElement = document.getElementById('text-input');
+
+    //Обработка события при нажатии на кнопку Enter - добавление комментария
+    document.addEventListener('keyup', (e) => {
+        if (e.code === 'Enter') {
+            buttonElement.click();
+        }
+    })
+
+    //Обработка события при нажатии на кнопку "Написать" - добавление комментария
+    buttonElement.addEventListener('click', () => {
+        buttonElement.disabled = true;
+        //Отправка данных в API
+        fetch(host, {
+            method: "POST",
+            body: JSON.stringify({
+                name: inputNameElement.value,
+                text: inputTextElement.value,
+            })
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((responseData) => {
+                // получили данные и рендерим их в приложении
+                comments = responseData.comments;
+                buttonElement.disabled = false;
+                getAPI();
+            });
+    })
+    //Обработка события при нажатии на кнопку лайк
+    const initButtonLikeListeners = () => {
+        const likeButtonElements = document.querySelectorAll('.like-button');
+        for (const likeButtonElement of likeButtonElements) {
+            likeButtonElement.addEventListener('click', () => {
+                const index = likeButtonElement.dataset.index;
+                comments[index].isLiked ? comments[index].likes-- : comments[index].likes++;
+                comments[index].isLiked = !comments[index].isLiked;
+                renderApp();
+            })
+        }
+    }
+
+    //Включение и выключение кнопки "Написать"
+    const buttonDisable = () => {
+        buttonElement.disabled = true;
+        inputNameElement.addEventListener('input', () => {
+            inputTextElement.addEventListener('input', () => {
+                buttonElement.disabled = false;
+            })
+        })
+    }
+
+    //Обработка события при нажатии на кнопку "Удалить"
+    const initDeleteButtonListeners = () => {
+        const deleteButtonElements = document.querySelectorAll('.delete-button');
+        for (const deleteButtonElement of deleteButtonElements) {
+            deleteButtonElement.addEventListener('click', () => {
+                const id = deleteButtonElement.dataset.index;
+                console.log(id);
+                //Обращение к API для удаления
+                // fetch(host + '/' + id, {
+                //     method: "DELETE"
+                // })
+                // .then((response) => {
+                //     return response.json()
+                // .then((responseData) => {
+                //         // получили данные и рендерим их в приложении
+                //         comments = responseData.comments;
+                //         renderComments();
+                //     });
+                // });
+
+            })
+        }
+    }
     initButtonLikeListeners();
-    initDeleteButtonListeners();
+    initDeleteButtonListeners()
 }
 
 //Запрос данных из API
@@ -69,97 +172,14 @@ const getAPI = () => {
                     isLiked: comment.isLiked,
                 }
             });
-            renderComments();
+            renderApp();
         })
 }
 
-//Обработка события при нажатии на кнопку лайк
-const initButtonLikeListeners = () => {
-    const likeButtonElements = document.querySelectorAll('.like-button');
-    for (const likeButtonElement of likeButtonElements) {
-        likeButtonElement.addEventListener('click', () => {
-            const index = likeButtonElement.dataset.index;
-            comments[index].isLiked ? comments[index].likes-- : comments[index].likes++;
-            comments[index].isLiked = !comments[index].isLiked;
-            renderComments();
-        })
-    }
-}
-
-//Включение и выключение кнопки "Написать"
-const buttonDisable = () => {
-    buttonElement.disabled = true;
-    inputNameElement.addEventListener('input', () => {
-        inputTextElement.addEventListener('input', () => {
-            buttonElement.disabled = false;
-        })
-    })
-}
 
 getAPI();
-buttonDisable();
-
-//Обработка события при нажатии на кнопку "Удалить"
-const initDeleteButtonListeners = () => {
-    const deleteButtonElements = document.querySelectorAll('.delete-button');
-    for (const deleteButtonElement of deleteButtonElements) {
-        deleteButtonElement.addEventListener('click', () => {
-            const id = deleteButtonElement.dataset.index;
-            console.log(id);
-            //Обращение к API для удаления
-            // fetch(host + '/' + id, {
-            //     method: "DELETE"
-            // })
-            // .then((response) => {
-            //     return response.json()
-            // .then((responseData) => {
-            //         // получили данные и рендерим их в приложении
-            //         comments = responseData.comments;
-            //         renderComments();
-            //     });
-            // });
-
-        })
-    }
-
-}
+renderApp();
 
 
-//Обработка события при нажатии на кнопку Enter - добавление комментария
-document.addEventListener('keyup', (e) => {
-    if (e.code === 'Enter') {
-        buttonElement.click();
-    }
-})
-
-//Обработка события при нажатии на кнопку "Написать" - добавление комментария
-buttonElement.addEventListener('click', () => {
-    //buttonElement.disabled = true;
-    //Отправка данных в API
-    fetch(host, {
-        method: "POST",
-        body: JSON.stringify({
-            name: inputNameElement.value,
-            text: inputTextElement.value,
-        })
-    })
-        .then((response) => {
-            return response.json()
-        })
-        .then((responseData) => {
-            // получили данные и рендерим их в приложении
-            comments = responseData.comments;
-            buttonElement.disabled = false;
-            getAPI();
-        });
-
-    renderComments();
-    initButtonLikeListeners();
-    initDeleteButtonListeners();
 
 
-    //очистка форм ввода
-    inputNameElement.value = '';
-    inputTextElement.value = '';
-    buttonElement.disabled = true;
-})
